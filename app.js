7d7a3e8f3a272daccbb31133260684834a259063
Sa194r <blackpanthers869@gmail.com>
2026-04-05 00:13:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -99,12 +99,12 @@ function renderPrices(sections){
         row.innerHTML = `
   <div class="priceitem__name">${it.name}</div>
   <div class="priceitem__price">
-    ${[
-  it.price,
-  it.c1,
-  it.c2,
-  it.c3
-].filter(Boolean).join(" / ")}
+    ${it.price 
+  ? it.price + " €"
+  : [it.c1 && it.c1 + " €", it.c2 && it.c2 + " €", it.c3 && it.c3 + " €"]
+      .filter(Boolean)
+      .join("   ")
+    }
   </div>
 `;
         if(it.note){
