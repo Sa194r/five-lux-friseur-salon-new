e5e64534a7a1731cf7ce8a1f5eeeae9d081a1fa3
Sa194r <blackpanthers869@gmail.com>
2026-04-09 22:50:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -284,3 +284,13 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+// Header effect
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
